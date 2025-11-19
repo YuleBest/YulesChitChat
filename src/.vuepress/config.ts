b5e67c6dd1adc568963cd6913d7449b35768ab73
YuleBest <yule-best@outlook.com>
2025-11-20 02:37:19 +0800

    refactor(字体): 删除字体以换取加载速度

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -20,35 +20,7 @@ export default defineUserConfig({
       "link",
       {
         rel: "stylesheet",
-        href: "/assets/fonts/woff2/PingFangSC-Light.woff2",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "stylesheet",
         href: "/assets/fonts/woff2/PingFangSC-Regular.woff2",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "stylesheet",
-        href: "/assets/fonts/woff2/PingFangSC-Medium.woff2",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "stylesheet",
-        href: "/assets/fonts/woff2/PingFangSC-Semibold.woff2",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "stylesheet",
-        href: "/assets/fonts/woff2/PingFangSC-Thin.woff2",
       },
     ],
   ],
