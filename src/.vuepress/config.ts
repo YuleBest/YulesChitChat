ccee410f0e4310eadbd624612a407d6826ae4df4
YuleBest <yule-best@outlook.com>
2025-11-23 17:34:36 +0800

    chore: 使用google fonts api代替本地字体

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,6 +9,22 @@ export default defineUserConfig({
   title: "于乐的碎碎念",
   description: "于乐的碎碎念丨Yule's Chit-Chat",
   lang: "zh-CN",
+  
+  head: [
+    // 字体
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
 
   alias: {
     "@VueDemo": path.resolve(__dirname, "components/VueDemo.vue"),
