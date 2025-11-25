12b12ed22408d1f13671f17e918e0cd13449a451
YuleBest <yule-best@outlook.com>
2025-11-25 15:52:18 +0800

    update sidebar

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -6,34 +6,61 @@ export default sidebar({
   // 笔记
   "/notes/": [
     "/",
-    "/textbook/",
     {
       text: "笔记",
       link: "/notes/",
       children: "structure",
     },
+    "/reprint/",
+    "/textbook/",
   ],
 
   // 前端 - 笔记
   "/notes/front-end/": [
     "/",
     "/notes/",
-    "/textbook/",
     {
       text: "前端",
       link: "/notes/front-end/",
       children: "structure",
     },
+    "/reprint/",
+    "/textbook/",
   ],
 
   // Markdown 教程 - 笔记
   "/notes/markdown/": [
     "/",
     "/notes/",
-    "/textbook/",
     {
       text: "于乐的 Markdown 教程",
       link: "/notes/markdown/",
+      children: "structure",
+    },
+    "/reprint/",
+    "/textbook/",
+  ],
+
+  // 转载
+  "/reprint/": [
+    "/",
+    "/notes/",
+    {
+      text: "转载",
+      link: "/reprint/",
+      children: "structure",
+    },
+    "/textbook/",
+  ],
+
+  // 教科书收录计划
+  "/textbook/": [
+    "/",
+    "/notes/",
+    "/reprint/",
+    {
+      text: "教科书收录计划",
+      link: "/textbook/",
       children: "structure",
     },
   ],
