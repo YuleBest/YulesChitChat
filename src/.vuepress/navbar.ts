023ae84840a3a0560ce921629a1e7d8be0ed4e01
YuleBest <yule-best@outlook.com>
2026-01-21 10:32:13 +0800

    update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,3 +1,12 @@
 import { navbar } from "vuepress-theme-hope";
 
-export default navbar(["/", "/category/", "/tag/", "/timeline/"]);
+export default navbar([
+  "/",
+  "/category/",
+  "/tag/",
+  "/timeline/",
+  {
+    text: "其他站点",
+    link: "/others",
+  },
+]);
---
+++ src/others.md
@@ -0,0 +1,17 @@
+---
+title: 其他站点
+
+pageInfo: false
+contributors: false
+icon: mdi:link
+prev: false
+next: false
+comment: false
+
+dir:
+  expanded: true
+---
+
+这里是我的其他站点，欢迎你也去看看
+
+- [小于工具箱 | Tool.Yule.ink](https://tool.yule.ink)
