a5b880eeb5b2ce61eb1886a59858cc1b623e406f
Yule <yule-best@outlook.com>
2026-03-31 06:36:50 +0800

    字体本地化

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,6 +1,9 @@
 import { defineClientConfig } from "vuepress/client";
 import VueDemo2 from "./components/VueDemo2.vue";
 
+import "@fontsource-variable/inter";
+import "@fontsource-variable/jetbrains-mono";
+
 export default defineClientConfig({
   enhance({ app }) {
     // 全局注册组件
